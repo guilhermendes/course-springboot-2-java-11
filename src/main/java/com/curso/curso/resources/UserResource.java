47dad39e7acf09b93e2dd42706719f371b4bba0e
Guilherme <guiweb85@gmail.com>
2022-12-07 13:20:20 -0300

    user entity and resource

---
+++ src/main/java/com/curso/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.curso.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAl(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "1324");
+		return ResponseEntity.ok().body(u);
+	}
+}
