d5f9b2359007c74ea5dc473a36bb268edf3dbb60
Guilherme <guiweb85@gmail.com>
2022-12-12 14:11:42 -0300

    create

--- src/main/java/com/curso/curso/resources/UserResource.java
+++ src/main/java/com/curso/curso/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<User> findAl(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "123456789");
+		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "132465");
 		return ResponseEntity.ok().body(u);
 	}
 }
