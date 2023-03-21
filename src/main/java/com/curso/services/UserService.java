10290bb8a465ef385b0c95b25d49fb9acc119ac4
Guilherme <guiweb85@gmail.com>
2023-03-21 10:51:27 -0300

    319. Tratamento de exceção - update

--- src/main/java/com/curso/services/UserService.java
+++ src/main/java/com/curso/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import com.curso.services.exceptions.DatabaseException;
 import com.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,10 +45,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj){
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-}
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+	}
 
 	private void updateData(User entity, User obj) {
 		entity.setNome(obj.getNome());
